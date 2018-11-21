d01118a3a3dcb5250aabe2b6cd47755615cd9a8f
tomer <tomer@HOME>
2018-11-21 22:02:00 +0200

    hghgh

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		 
+		int tbd=4; 
+		System.out.println("Hello World Tomtfdf d");
+		System.out.println(tbd);
+		
+	}
+	
+	
+	
+	
+}
